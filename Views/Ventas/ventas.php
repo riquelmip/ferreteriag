cda6958e1c29855a71d3b90ac0bcdb879c7e2d36
Riccieri Riquelmi Alvarado Palacios <ap18011@ues.edu.sv>
2021-12-27 16:15:49 -0600

    ayuda en nuevaventa

--- Views/Ventas/ventas.php
+++ Views/Ventas/ventas.php
@@ -14,8 +14,9 @@
         </div>
        
         <ul class="app-breadcrumb breadcrumb">
+          
           <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-          <li class="breadcrumb-item"><a href="<?= base_url(); ?>/compra"><?= $data['page_title'] ?></a></li>
+          <li class="breadcrumb-item"><a href="<?= base_url(); ?>/ventas"><?= $data['page_title'] ?></a></li>
         </ul>
       </div>
         <div class="row">
