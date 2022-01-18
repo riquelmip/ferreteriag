93aaa1143f154906d2084f0880c26b3e3bb52625
w <dm18019@ues.edu.sv>
2022-01-18 17:07:10 -0600

    Cambio

--- Views/Backup/Backup.php
+++ Views/Backup/Backup.php
@@ -4,7 +4,8 @@
 		include('Views/Backup/Fuction_Backup.php');
 
 		echo backup_tables('ec2-18-223-108-73.us-east-2.compute.amazonaws.com','will','will','db_ferreteria');
-
+		var_dump("entro");
+		die();
 		$fecha=date("Y-m-d");
 		header("Content-disposition: attachment; filename=db-ferreteria-".$fecha.".sql");
 		header("Content-type: MIME");
