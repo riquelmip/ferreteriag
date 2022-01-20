3d56927fd3c3634e6cff8c8f947b455097a1147c
w <dm18019@ues.edu.sv>
2022-01-19 21:17:30 -0600

    Cambios

--- Views/Acercade/acercade.php
+++ Views/Acercade/acercade.php
@@ -32,7 +32,7 @@ getModal('modalCargos', $data);
                 <div class="card-header" style="font-size: x-large; text-align: left;">Manuales
                         </div>  </div>
                 <div class="justify-content-md-end">
-                    <a type="button" class="btn btn-light ms-4 mb-2 " onclick="window.open('Assets/images/ayuda/Manualdeinstalacion.pdf')" style="font-size: larger; background-color: green; border-color: black; color: white; "><i class=" bi bi-info-circle"></i> Manual de Instalación</a>
+                    <a type="button" class="btn btn-light ms-4 mb-2 " onclick="window.open('Assets/images/ayuda/ManualdeInstalacion.pdf')" style="font-size: larger; background-color: green; border-color: black; color: white; "><i class=" bi bi-info-circle"></i> Manual de Instalación</a>
                     <a type="button" class="btn btn-light ms-4 mb-2 " onclick="window.open('Assets/images/ayuda/ManualdeUsuario.pdf')" style="font-size: larger; background-color: red; border-color: black; color: white; "><i class=" bi bi-info-circle"></i> Manual de Usuario</a>
                     <a type="button" class="btn btn-light ms-4 mb-2 " onclick="window.open('Assets/images/ayuda/ManualdeUsuario.pdf')" style="font-size: larger; background-color: blue; border-color: black; color: white; "><i class=" bi bi-info-circle"></i> Manual del Programador</a>
                 </div>
