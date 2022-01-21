8004486cd4a3dc8529d793f54c7b433caf8e3ff0
w <dm18019@ues.edu.sv>
2022-01-20 22:14:27 -0600

    12

--- Assets/js/functions_dashboard.js
+++ Assets/js/functions_dashboard.js
@@ -7,25 +7,23 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 
+
 function cargar_datos(){
+  divLoading.style.display = "flex";
+  var datos = {"consultar_info":"si_consultala"}
   $.ajax({
       dataType: "json",
       method: "POST",
-      url: base_url+'/Dashboard/getDatos',
+      url: base_url+"/Dashboard/getDatos",
+      data : datos,
   }).done(function(json) {
-    console.log(json);
-
-
    // document.getElementById('totalCreditos').innerHTML =  "$"+json['totalVenta']['totalVenta'];
 
   //  document.getElementById('totalVentas').innerHTML =  "$"+json['totalCompra']['totalCompra'];
 
-
   }).fail(function(){
 
   }).always(function(){
-    
+      divLoading.style.display = "none";
   });
-  
-};
-
+}
