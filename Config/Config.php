5f28d407acb7184d97980376cdbef710dfdc2ecd
Emerson Vladimir Palacios <93158771+vlady04@users.noreply.github.com>
2022-05-31 14:55:25 -0600

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,45 +1,19 @@
 <?php 
 	
-	//define("BASE_URL", "http://localhost/ferreteria/");
-//const BASE_URL2 = "http://ferreteria.hinakocc.engineer/consultas/";
 
-	//const BASE_URL = "http://localhost/ferreteriag";
-	const BASE_URL = "http://ferreteria.hinakocc.engineer";
-//const BASE_URL2 = "http://localhost/ferreteriag/consultas/";
-const BASE_URL2 = "http://ferreteria.hinakocc.engineer/consultas/";
+	const BASE_URL = "http://localhost/ferreteriag";
+	const BASE_URL2 = "http://localhost/ferreteriag/consultas/";
+
 	//Zona horaria
 	date_default_timezone_set('America/El_Salvador');
 	
-
-	/*/Datos de conexión a Base de Datos
-	const DB_HOST = "ec2-3-133-114-204.us-east-2.compute.amazonaws.com";
-	const DB_NAME = "db_ferreteria";
-	const DB_USER = "will";
-	const DB_PASSWORD = "will";
-	const DB_CHARSET = "utf8";*/
-	
-
-	//Datos de conexión a Base de Datos
-	const DB_HOST = "ec2-18-191-71-210.us-east-2.compute.amazonaws.com";
-	const DB_NAME = "db_ferreteria";
-	const DB_USER = "will";
-	const DB_PASSWORD = "will";
-	const DB_CHARSET = "utf8";
-	/*
-
 
 	const DB_HOST = "localhost" ;
 	const DB_NAME = "db_ferreteria";
 	const DB_USER = "root";
 	const DB_PASSWORD = "";
-	const DB_CHARSET = "utf8";*/
+	const DB_CHARSET = "utf8";
 
-
-	//const DB_HOST = "localhost";
-	//const DB_NAME = "comuesco_ferreteriagranadeno";
-	//const DB_USER = "comuesco_ferreteriagranadeno";
-	//const DB_PASSWORD = "4@#;!@w++Av_";
-	//const DB_CHARSET = "utf8";
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
 	const SPD = ".";
@@ -53,9 +27,6 @@ const BASE_URL2 = "http://ferreteria.hinakocc.engineer/consultas/";
 	const EMAIL_REMITENTE = "ferreteriagranadenio12@gmail.com";
 	const NOMBRE_EMPESA = "Ferreteria Granadeño";
 	const WEB_EMPRESA = "ferreteria_granadeño.com";
-	
-
-		// PARA EL BACKUP C SUPONE XD
 
 
  ?>
